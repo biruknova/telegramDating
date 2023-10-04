79a9d244e57525c9c04d5c5407201abe5f4d669d
biruknova <biruknova1@gmail.com>
2023-10-04 15:10:51 +0300

    bottom nav adjestment

--- src/components/cards/profileCard.jsx
+++ src/components/cards/profileCard.jsx
@@ -4,6 +4,8 @@ import HeartIcon from "../icons/Heart";
 const ProfileCard = ({ name, age, bio, img, onClick }) => {
   const textColor = window.Telegram.WebApp.text_color;
   const hintColor = window.Telegram.WebApp.hint_color;
+
+  alert(textColor, hintColor);
   return (
     <section className="w-full max-w-[500px]   mx-auto flex flex-col items-center">
       <div className="flex flex-col items-center  rounded-md overflow-hidden bg-white dark:bg-[#273552]">
