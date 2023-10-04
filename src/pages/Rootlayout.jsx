ff88f258d8b13581c2f9b23dc8558cd9ac43e37f
biruknova <biruknova1@gmail.com>
2023-10-04 15:49:53 +0300

    telegram color adjustment

--- src/pages/Rootlayout.jsx
+++ src/pages/Rootlayout.jsx
@@ -8,7 +8,7 @@ const Rootlayout = () => {
   const bgColor = window.Telegram.WebApp.bg_color;
   const secondaryBgColor = window.Telegram.WebApp.secondary_bg_color;
 
-  alert(window.Telegram.WebApp.is_bot);
+  alert(window.Telegram.WebApp.initData);
 
   return (
     <div className="flex flex-col min-h-screen w-full max-w-[600px] mx-auto">
