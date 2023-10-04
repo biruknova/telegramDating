043f163adb4d2d9627b7bd4a49702e43fdf4f49a
biruknova <biruknova1@gmail.com>
2023-10-04 17:50:32 +0300

    telegram color adjustment

--- src/components/cards/profileCard.jsx
+++ src/components/cards/profileCard.jsx
@@ -4,8 +4,8 @@ import HeartIcon from "../icons/Heart";
 const ProfileCard = ({ name, age, bio, img, onClick }) => {
   return (
     <section className="w-full max-w-[500px] mx-auto flex flex-col items-center">
-      <div className="flex flex-col items-center  rounded-md overflow-hidden space-y-5 divide-y divide-slate-100 dark:divide-gray-800">
-        <div className="w-full flex flex-col">
+      <div className="flex flex-col items-center  rounded-md overflow-hidden space-y-2">
+        <div className="w-full flex flex-col space-y-5">
           <div className="w-full relative">
             <img src={img} alt="profile" className="w-full" />
             <div className="flex flex-col items-start text-white absolute bottom-0 left-0 p-4 bg-gradient-to-b from-transparent via-black/40 to-black/60 w-full">
@@ -13,11 +13,12 @@ const ProfileCard = ({ name, age, bio, img, onClick }) => {
               <h2 className="drop-shadow font-semibold">Age: {age}</h2>
             </div>
           </div>
-          <div className="px-4 flex flex-col w-full space-y-1 text-sm px-4">
+          <div className="px-4 flex flex-col w-full space-y-1 text-sm">
             <p className={`text-gray-700 dark:text-white`}>{bio}</p>
             <h1 className={`text-gray-400 text-xs`}>Bio</h1>
           </div>
         </div>
+        <hr className="border-t border-slate-100 dark:border-gray-800 mx-4 " />
 
         <div className="flex space-x-4 px-4 w-full">
           <button
