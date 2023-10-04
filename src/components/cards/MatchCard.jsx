3e627d370ba89d069a9d3c9368807062c071a6b1
biruknova <biruknova1@gmail.com>
2023-10-04 19:18:45 +0300

    matches page progress

--- src/components/cards/MatchCard.jsx
+++ src/components/cards/MatchCard.jsx
@@ -2,7 +2,7 @@ const MatchCard = ({ name, age }) => {
   return (
     <div className="w-full flex items-center py-2 space-x-5 ">
       <div className="w-[60px] h-[60px] bg-white rounded-full"></div>
-      <div className="text-white">
+      <div className="text-white h-[60px] flex flex-col justify-center border-y border-black/20">
         <h1 className=" text-lg font-medium">{name}</h1>
         <p className="text-sm">Age: {age}</p>
       </div>
