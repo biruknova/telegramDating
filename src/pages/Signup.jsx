8f80f41709c196bbe18dbd42e1dafa249f750d9d
biruknova <biruknova1@gmail.com>
2023-10-06 15:28:17 +0300

    signup inputs progress

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -41,25 +41,6 @@ const SignupPage = () => {
       className="bg-slate-700 flex flex-col justify-center min-h-screen p-3"
     >
       <div className="w-full flex flex-col space-y-5">
-        <div className="flex flex-col">
-          <label style={{ color: ageIsFocused ? btnColor : hintColor }}>
-            Age
-          </label>
-          <input
-            type="number"
-            style={{
-              borderBottom: `1px solid ${ageIsFocused ? btnColor : hintColor}`,
-              color: txtColor,
-            }}
-            className={`outline-none py-1 b bg-transparent transition-colors duration-200`}
-            onFocus={() => {
-              focusHandler("age");
-            }}
-            onBlur={() => {
-              blurHandler("age");
-            }}
-          ></input>
-        </div>
         <div className="relative">
           <input
             type="text"
@@ -69,6 +50,12 @@ const SignupPage = () => {
             style={{
               border: `1px solid ${nameIsFocused ? btnColor : hintColor}`,
               color: txtColor,
+            }}
+            onFocus={() => {
+              focusHandler("name");
+            }}
+            onBlur={() => {
+              blurHandler("name");
             }}
           />
           <label
@@ -80,6 +67,34 @@ const SignupPage = () => {
             className="absolute text-sm  duration-200 transform -translate-y-4 scale-75 top-1.5 z-10 origin-[0] px-2 peer-focus:px-2  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1.5 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
           >
             Full Name
+          </label>
+        </div>
+        <div className="relative">
+          <input
+            type="number"
+            id="full_name"
+            className="block px-2.5 pb-2.5 pt-4 w-full text-sm  bg-transparent rounded-lg  appearance-none  outline-none ring-0  peer"
+            placeholder=" "
+            style={{
+              border: `1px solid ${ageIsFocused ? btnColor : hintColor}`,
+              color: txtColor,
+            }}
+            onFocus={() => {
+              focusHandler("age");
+            }}
+            onBlur={() => {
+              blurHandler("age");
+            }}
+          />
+          <label
+            htmlFor="full_name"
+            style={{
+              color: ageIsFocused ? btnColor : hintColor,
+              backgroundColor: bgColor,
+            }}
+            className="absolute text-sm  duration-200 transform -translate-y-4 scale-75 top-1.5 z-10 origin-[0] px-2 peer-focus:px-2  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1.5 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+          >
+            Age
           </label>
         </div>
         <div className="flex flex-col">
