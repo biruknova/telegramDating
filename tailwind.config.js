2095a780ba312177099e3cc0d1893dc99afe690a
biruknova <biruknova1@gmail.com>
2023-10-06 11:40:39 +0300

    signup page progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = window.Telegram.WebApp.themeParams;
+
+const {
+  bg_color: bgColor,
+  text_color: txtColor,
+  hint_color: hintColor,
+  button_color: btnColor,
+  button_text_color: btnTxtColor,
+} = colors;
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      btnColor: btnColor,
+      bgColor: bgColor,
+      txtColor: txtColor,
+      hintColor: hintColor,
+      btnTxtColor: btnTxtColor,
+    },
   },
   plugins: [],
 };
