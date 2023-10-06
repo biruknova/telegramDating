a90d41754a673ca3adaa8c3c12d1935376b4ecbc
biruknova <biruknova1@gmail.com>
2023-10-07 01:29:09 +0300

    finalizing signup

--- src/pages/MatchProfile.jsx
+++ src/pages/MatchProfile.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 const MatchProfile = () => {
   const param = useParams();
-  window.Telegram.WebApp.show();
+  window.Telegram.WebApp.BackButton.show();
   return <div>This is match profile page {param.matchId} </div>;
 };
 export default MatchProfile;
