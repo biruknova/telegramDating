fe05356358d76dcd207045fd31064e1776c7bfd6
biruknova <biruknova1@gmail.com>
2023-10-06 11:53:26 +0300

    signup page progress

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,23 +1,75 @@
+import { useState } from "react";
+
 import RadioToggle from "../components/Radios";
 
 const SignupPage = () => {
+  const [nameIsFocused, setNameIsFocused] = useState(false);
+  const [ageIsFocused, setAgeIsFocused] = useState(false);
+
+  const focusHandler = (lable) => {
+    if (lable === "name") {
+      setNameIsFocused(true);
+    } else {
+      setAgeIsFocused(true);
+    }
+  };
+
+  const blurHandler = (lable) => {
+    if (lable === "name") {
+      setNameIsFocused(false);
+    } else {
+      setAgeIsFocused(false);
+    }
+  };
+
   window.Telegram.WebApp.MainButton.show();
 
+  const colors = window.Telegram.WebApp.themeParams;
+
+  const {
+    bg_color: bgColor,
+    text_color: txtColor,
+    hint_color: hintColor,
+    button_color: btnColor,
+    button_text_color: btnTxtColor,
+    secondary_bg_color: secondaryBgColor,
+  } = colors;
+
+  console.log("colors", txtColor, hintColor, btnTxtColor);
   return (
-    <div className="bg-bgColor flex flex-col justify-center min-h-screen p-3">
+    <div
+      style={{ backgroundColor: bgColor }}
+      className="bg-slate-700 flex flex-col justify-center min-h-screen p-3"
+    >
       <div className="w-full flex flex-col space-y-5">
         <div className="flex flex-col">
-          <label>Full Name</label>
+          <label
+            style={{
+              color: nameIsFocused ? btnColor : secondaryBgColor,
+            }}
+          >
+            Full Name
+          </label>
           <input
             type="text"
-            className={`outline-none py-1 border-b border-black bg-transparent focus:border-btnColor  transition-colors duration-200`}
+            style={{
+              borderBottom: nameIsFocused ? btnColor : secondaryBgColor,
+            }}
+            className={`outline-none py-1 bg-transparent transition-colors duration-200`}
+            onFocus={focusHandler}
+            onBlur={blurHandler}
           ></input>
         </div>
         <div className="flex flex-col">
-          <label>Age</label>
+          <label style={{ color: ageIsFocused ? btnColor : secondaryBgColor }}>
+            Age
+          </label>
           <input
             type="number"
-            className={`outline-none py-1 border-b border-black bg-transparent focus:border-btnColor  transition-colors duration-200`}
+            style={{ borderBottom: ageIsFocused ? btnColor : secondaryBgColor }}
+            className={`outline-none py-1 b bg-transparent transition-colors duration-200`}
+            onFocus={focusHandler}
+            onBlur={blurHandler}
           ></input>
         </div>
         <div className="flex flex-col">
