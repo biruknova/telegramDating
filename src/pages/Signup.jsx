19b93cc825cbba6b6aa8b0609afd62664f5fd7e0
biruknova <biruknova1@gmail.com>
2023-10-07 00:16:39 +0300

    finalizing signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -51,7 +51,7 @@ const SignupPage = () => {
     if (lable === "name") {
       setNameIsFocused(false);
     } else {
-      if (formData.age !== "" && formData.age.length < 2) {
+      if (formData.age !== "" && formData.age < 16) {
         setBelowAgeLimit(true);
       } else {
         setBelowAgeLimit(false);
@@ -221,7 +221,7 @@ const SignupPage = () => {
                 nameIsFocused || formData.name !== "" ? btnColor : hintColor,
               backgroundColor: bgColor,
             }}
-            className="absolute text-sm  duration-100 transform -translate-y-4 scale-75 top-1.5 z-10 origin-[0] px-2 peer-focus:px-2  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1.5 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+            className="absolute text-sm  transform -translate-y-4 scale-75 top-1.5 z-10 origin-[0] px-2 peer-focus:px-2  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1.5 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
           >
             Full Name
           </label>
