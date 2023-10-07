c09c4357608f575fe2f936cda0758fa30ca7b439
biruknova <biruknova1@gmail.com>
2023-10-08 02:07:24 +0300

    getting matches

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,123 @@
-const ProfilePage = () => {
-  return <div className="p-4"> This is Profile page </div>;
-};
+import { useEffect, useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
+import context from "../store/context";
+
+const ProfilePage = () => {
+  const navigate = useNavigate();
+
+  const BackButton = window.Telegram.WebApp.BackButton;
+
+  BackButton.show();
+
+  useEffect(() => {
+    const handleBack = () => {
+      navigate("/matches");
+    };
+
+    BackButton.onClick(handleBack);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      BackButton.offClick(handleBack);
+      BackButton.hide();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const colors = window.Telegram.WebApp.themeParams;
+
+  const {
+    bg_color: bgColor,
+    text_color: txtColor,
+    hint_color: hintColor,
+    // button_color: btnColor,
+    secondary_bg_color: secondaryBgColor,
+  } = colors;
+
+  const { matches } = useContext(context);
+
+  const params = useParams();
+  const matchId = params.matchId;
+
+  console.log(matchId);
+
+  const match = matches.find((match) => match.id === Number(matchId));
+  console.log(match);
+
+  return (
+    <div
+      style={{ backgroundColor: secondaryBgColor }}
+      className=" w-full min-h-screen flex flex-grow pt-[130px] z-50"
+    >
+      <div
+        style={{ backgroundColor: bgColor }}
+        className="w-full flex flex-col flex-grow rounded-t-xl"
+      >
+        <div className="-mt-[50px] flex flex-col items-center space-y-6 w-full">
+          <div className="flex flex-col items-center space-y-2 w-full">
+            <div
+              style={{ border: `5px solid ${secondaryBgColor}` }}
+              className="rounded-full w-[130px] h-[130px] rounded-full bg-red-200 overflow-hidden"
+            >
+              <img
+                src={
+                  match && match.photos
+                    ? match.photos[0]
+                    : "https://picsum.photos/200"
+                }
+                alt="match profile"
+              />
+            </div>
+            <div className="flex flex-col space-y-1.5 items-center text-center w-[85%]">
+              <h1
+                style={{ color: txtColor }}
+                className="text-xl font-semibold min-w-max"
+              >
+                {match ? match.name : ""}
+              </h1>
+            </div>
+          </div>
+          <div
+            style={{ color: txtColor }}
+            className="w-full flex flex-col divide-y dark:divide-black/30 divid-slate-100 pl-5 text-sm mt-3"
+          >
+            {match && match.tg_username && (
+              <div className="pr-5 py-3 space-y-1 text-start">
+                <h1>@{match.tg_username}</h1>
+                <p style={{ color: hintColor }} className="text-xs">
+                  Username
+                </p>
+              </div>
+            )}
+            {match && match.bio && (
+              <div className="pr-5 py-3 space-y-1 text-start">
+                <h1>{match.bio}</h1>
+                <p style={{ color: hintColor }} className="text-xs">
+                  Bio
+                </p>
+              </div>
+            )}
+            {match && match.age && (
+              <div className="pr-5 py-3 space-y-1 text-start">
+                <h1>{match.age}</h1>
+                <p style={{ color: hintColor }} className="text-xs">
+                  Age
+                </p>
+              </div>
+            )}
+            {match && match.gender && (
+              <div className="pr-5 py-3 space-y-1 text-start">
+                <h1>{match.gender.name} </h1>
+                <p style={{ color: hintColor }} className="text-xs">
+                  Gender
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default ProfilePage;
