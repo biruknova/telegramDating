f934df4b50bbb0510efd731422fddec191f9211f
biruknova <biruknova1@gmail.com>
2023-10-07 03:14:34 +0300

    image preloading

--- src/components/ImagePreloader.jsx
+++ src/components/ImagePreloader.jsx
@@ -1,14 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ImagePreloader = ({ imageUrls }) => {
+  const [loadedCount, setLoadedCount] = useState(0);
+
   useEffect(() => {
+    let loadedImages = 0;
+
     // Create an image element for each image URL to preload them all
     imageUrls.forEach((imageUrl) => {
       const img = new Image();
+
+      img.onload = () => {
+        loadedImages++;
+        if (loadedImages === imageUrls.length) {
+          // All images have loaded
+          setLoadedCount(loadedImages);
+        }
+      };
+
+      img.onerror = () => {
+        // Handle image load errors, if needed
+        loadedImages++;
+        if (loadedImages === imageUrls.length) {
+          // All images have finished loading (including errors)
+          setLoadedCount(loadedImages);
+        }
+      };
+
       img.src = imageUrl;
     });
   }, [imageUrls]);
 
-  return null; // This component doesn't render anything
+  return (
+    <div>
+      {/* Optionally display the loadedCount if needed */}
+      Images Loaded: {loadedCount}
+    </div>
+  );
 };
+
 export default ImagePreloader;
