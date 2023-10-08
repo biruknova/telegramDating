3e8f90db15ebb5c06e78db5ab0f49a8cddd5bc80
biruknova <biruknova1@gmail.com>
2023-10-08 15:42:41 +0300

    edit profile ui scheme

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -97,7 +97,51 @@ const SignupPage = () => {
     }
   };
 
-  // Initialize the initial state and previous state as false
+  // // Initialize the initial state and previous state as false
+  // const [prevFieldsAreFilled, setPrevFieldsAreFilled] = useState(false);
+
+  // const areAllValuesFilled = () => {
+  //   const nameIsValid = formData.name !== "";
+  //   const genderIsValid = formData.gender_id !== "";
+  //   const ageIsValid = formData.age >= 16;
+  //   const dataIsValid = formData.tg_data !== "";
+
+  //   const fieldsAreFilled =
+  //     nameIsValid && genderIsValid && ageIsValid && dataIsValid;
+
+  //   // Check if the current state is different from the previous state
+  //   if (fieldsAreFilled !== prevFieldsAreFilled) {
+  //     setPrevFieldsAreFilled(fieldsAreFilled); // Update the previous state
+
+  //     console.log("fields are filled", fieldsAreFilled);
+
+  //     if (fieldsAreFilled) {
+  //       MainButton.show();
+  //       MainButton.onClick(registerUser);
+  //     } else {
+  //       MainButton.hide();
+  //       MainButton.offClick(registerUser);
+  //     }
+  //   }
+  // };
+
+  // // useEffect to update the initial and previous state when formData changes
+  // useEffect(() => {
+  //   const nameIsValid = formData.name !== "";
+  //   const genderIsValid = formData.gender_id !== "";
+  //   const ageIsValid = formData.age >= 16;
+  //   const dataIsValid = formData.tg_data !== "";
+
+  //   setPrevFieldsAreFilled(
+  //     nameIsValid && genderIsValid && ageIsValid && dataIsValid
+  //   );
+  // }, [formData]);
+
+  // useEffect(() => {
+  //   areAllValuesFilled();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [formData]);
+
   const [prevFieldsAreFilled, setPrevFieldsAreFilled] = useState(false);
 
   const areAllValuesFilled = () => {
@@ -109,11 +153,8 @@ const SignupPage = () => {
     const fieldsAreFilled =
       nameIsValid && genderIsValid && ageIsValid && dataIsValid;
 
-    // Check if the current state is different from the previous state
     if (fieldsAreFilled !== prevFieldsAreFilled) {
-      setPrevFieldsAreFilled(fieldsAreFilled); // Update the previous state
-
-      console.log("fields are filled", fieldsAreFilled);
+      setPrevFieldsAreFilled(fieldsAreFilled);
 
       if (fieldsAreFilled) {
         MainButton.show();
@@ -125,20 +166,39 @@ const SignupPage = () => {
     }
   };
 
-  // useEffect to update the initial and previous state when formData changes
-  useEffect(() => {
-    const nameIsValid = formData.name !== "";
-    const genderIsValid = formData.gender_id !== "";
-    const ageIsValid = formData.age >= 16;
-    const dataIsValid = formData.tg_data !== "";
-
-    setPrevFieldsAreFilled(
-      nameIsValid && genderIsValid && ageIsValid && dataIsValid
-    );
-  }, [formData]);
-
   useEffect(() => {
     areAllValuesFilled();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formData, prevFieldsAreFilled]);
+
+  // Initialize prevFieldsAreFilled when component mounts
+  useEffect(() => {
+    setPrevFieldsAreFilled(false);
+  }, []);
+
+  // Cleanup when component unmounts
+  useEffect(() => {
+    return () => {
+      MainButton.offClick(registerUser);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Attach event handlers when component mounts
+  useEffect(() => {
+    if (prevFieldsAreFilled) {
+      MainButton.show();
+      MainButton.onClick(registerUser);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [prevFieldsAreFilled]);
+
+  // Remove event handlers when fields change
+  useEffect(() => {
+    if (!prevFieldsAreFilled) {
+      MainButton.hide();
+      MainButton.offClick(registerUser);
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [formData]);
 
