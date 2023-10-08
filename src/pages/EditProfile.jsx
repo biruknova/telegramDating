e35dc85ea2bcfb33dfee4549d23a0b0eeb5e16b6
biruknova <biruknova1@gmail.com>
2023-10-08 03:10:14 +0300

    getting matches

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -10,7 +10,7 @@ const EditProfile = () => {
 
   useEffect(() => {
     const handleBack = () => {
-      navigate("/matches");
+      navigate("/edit-profile");
     };
 
     BackButton.onClick(handleBack);
