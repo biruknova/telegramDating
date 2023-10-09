4abe4bfe7d0acc9ad67b1db11353295700b44208
biruknova <biruknova1@gmail.com>
2023-10-09 19:57:55 +0300

    Home section Profile card shimmer addition

--- src/components/shimmers/ProfileCardShimmer.jsx
+++ src/components/shimmers/ProfileCardShimmer.jsx
@@ -11,19 +11,41 @@ const ProfileCardShimmer = () => {
         <div className="w-full flex flex-col space-y-5">
           <div
             style={{ height: "100vw" }}
-            className="w-full relative bg-slate-100 dark:bg-white"
+            className={`w-full relative ${
+              mode ? "bg-white/10" : "bg-slate-100"
+            } `}
           >
             <div className="flex flex-col items-start text-white absolute bottom-0 left-0 p-4 w-full">
               <div className="flex items-center space-x- w-full">
-                <div className="h-3.5 w-1/2 dark:animate-waiter-dark animate-waiter-light rounded-full"></div>
+                <div
+                  className={`h-3.5 w-1/2 ${
+                    mode ? "animate-waiter-dark" : "animate-waiter-light"
+                  }  rounded-full`}
+                ></div>
               </div>
-              <div className="h-2.5 w-1/4 dark:animate-waiter-dark animate-waiter-light rounded-full mt-2"></div>
+              <div
+                className={`h-2.5 w-1/4 dark:animate-waiter-dark animate-waiter-ligh${
+                  mode ? "animate-waiter-dark" : "animate-waiter-light"
+                }  rounded-full mt-2`}
+              ></div>
             </div>
           </div>
           <div className="px-4 flex flex-col w-full space-y-2 text-sm min-h-[80px]">
-            <div className="h-2.5 w-full dark:animate-waiter-dark animate-waiter-light bg-yellow-500 rounded-full"></div>
-            <div className="h-2.5 w-full dark:animate-waiter-dark animate-waiter-light rounded-full"></div>
-            <div className="h-2.5 w-1/2 dark:animate-waiter-dark animate-waiter-light rounded-full"></div>
+            <div
+              className={`h-2.5 w-full ${
+                mode ? "animate-waiter-dark" : "animate-waiter-light"
+              }  rounded-full`}
+            ></div>
+            <div
+              className={`h-2.5 w-full ${
+                mode ? "animate-waiter-dark" : "animate-waiter-light"
+              } rounded-full`}
+            ></div>
+            <div
+              className={`h-2.5 w-1/2 ${
+                mode ? "animate-waiter-dark" : "animate-waiter-light"
+              }  rounded-full`}
+            ></div>
           </div>
         </div>
         <div className="px-4 w-full">
@@ -36,7 +58,11 @@ const ProfileCardShimmer = () => {
               mode ? "animate-waiter-dark" : "animate-waiter-light"
             }  `}
           ></button>
-          <button className="w-1/2 h-[45px] rounded  flex justify-center items-center  dark:animate-waiter-dark animate-waiter-light"></button>
+          <button
+            className={`w-1/2 h-[45px] rounded  flex justify-center items-center ${
+              mode ? "animate-waiter-dark" : "animate-waiter-light"
+            }  `}
+          ></button>
         </div>
       </div>
     </section>
